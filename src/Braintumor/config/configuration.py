0138b4c8bc78d8744314d3535be7cf09c8971856
Krrishna Jorige <Krrishna.Jorige@primeroedge.com>
2024-11-07 17:04:27 -0600

    Evaluation Completed

--- src/Braintumor/config/configuration.py
+++ src/Braintumor/config/configuration.py
@@ -1,8 +1,11 @@
 from Braintumor.constants import *
 from Braintumor.utils.common import read_yaml, create_directories
-from Braintumor.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig)
+from Braintumor.entity.config_entity import (DataIngestionConfig,
+                                             PrepareBaseModelConfig,
+                                             PrepareCallbacksConfig,
+                                             TrainingConfig,EvaluationConfig)
 import os
-
+from pathlib import Path
 class ConfigurationManager:
     def __init__(
         self,
@@ -87,3 +90,13 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/brain_tumor_dataset"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
--- src/Braintumor/entity/config_entity.py
+++ src/Braintumor/entity/config_entity.py
@@ -27,6 +27,7 @@ class PrepareCallbacksConfig:
     tensorboard_root_log_dir: Path
     checkpoint_model_filepath: Path
 
+
 @dataclass(frozen=True)
 class TrainingConfig:
     root_dir: Path
@@ -37,3 +38,12 @@ class TrainingConfig:
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    params_image_size: list
+    params_batch_size: int
